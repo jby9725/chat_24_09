790254f49f6eda11d22f77ac3b3b8091d5283a38
jby9725 <jby9725@gmail.com>
2024-09-06 13:58:09 +0900

    메세지 작성시 작성자, 내용을 JSON형태로 전달, 포스트맨으로 body에 실어 요청 보내면 잘 받는것 확인

--- src/main/java/com/koreait/exam/chat_24_09/ChatController.java
+++ src/main/java/com/koreait/exam/chat_24_09/ChatController.java
@@ -1,10 +1,7 @@
 package com.koreait.exam.chat_24_09;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,20 +12,31 @@ public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
-    public record writeChatMessageResponse(long id) {
+//    @AllArgsConstructor
+//    @Getter
+//    public static class writeMessageRequest {
+//        private final String authorName;
+//        private final String content;
+//    }
+
+    public record writeMessageRequest(String authorName, String content) {
+    }
+
+
+    public record writeMessageResponse(long id) {
 
     }
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<writeChatMessageResponse> writeMessage() {
-        ChatMessage message = new ChatMessage("홍길동", "안녕");
+    public RsData<writeMessageResponse> writeMessage(@RequestBody writeMessageRequest req) {
+        ChatMessage message = new ChatMessage(req.authorName, req.content);
 
         chatMessages.add(message);
 
         return new RsData<>("S-1",
                 "메세지가 작성됨",
-                new writeChatMessageResponse(message.getId())
+                new writeMessageResponse(message.getId())
         );
     }
 
